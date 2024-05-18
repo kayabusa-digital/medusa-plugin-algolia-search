17a672eadff589b4c8f1dd8690c089364f820754
Soon Chai <256in4@gmail.com>
2024-05-18 04:16:52 +0300

    feat: extend type to process multiple indexes for each entity types of medusa presented

--- src/types.ts
+++ src/types.ts
@@ -6,18 +6,24 @@ export type SearchOptions = {
 	additionalOptions: Record<string, unknown>
 }
 
+export type IndexSettingsExtended = SearchTypes.IndexSettings & {
+	indexSettings: {
+		indexName: string // algolia index name in case you are using not `products` name for index, this property are value for copy(Transformer|Filter)From properties, if you are using `products` naming and want to copy methods from this settings, name it `products`. Must be unique value
+	}
+	copyTransformerFrom?: string // indexName of existing IndexSettingsExtended array, transformer of which will be used, otherwise itself declared transformer be used
+	copyFilterFrom?: string // indexName of existing IndexSettingsExtended array, filter of which will be used, otherwise itself declared filter be used
+	filter?: (document: any) => boolean
+}
+
+export type IndexTypes = 'products' | string
+
 export type AlgoliaPluginOptions = {
 	applicationId: string
 	adminApiKey: string
 	/**
 	 * Index settings
 	 */
-	settings?: {
-		[key: string]: SearchTypes.IndexSettings & {
-			indexSettings: {
-				indexName: string
-			}
-			filter?: (document: any) => boolean
-		}
+	settings: {
+		[key in IndexTypes]: IndexSettingsExtended[]
 	}
 }
